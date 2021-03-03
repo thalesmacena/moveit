d5294e7718462d7e419c08e1b2d8528f50a70238
Thales Macena <thalesmacena@gmail.com>
2021-03-02 23:33:06 -0300

    add home page

---
+++ src/pages/moveit.tsx
@@ -0,0 +1,75 @@
+import { GetServerSideProps } from 'next';
+import Head from 'next/head';
+import { ChallengeBox } from '../components/ChallengeBox';
+import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Countdown } from '../components/Countdown';
+import { ExperienceBar } from '../components/ExperienceBar';
+import { Profile } from '../components/Profile';
+import { ChallengesProvider } from '../contexts/ChallengesContext';
+import { CountdownProvider } from '../contexts/CountdownContext';
+import { ProfileProvider } from '../contexts/ProfileContext';
+import styles from '../styles/pages/Home.module.css';
+
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+  username: string;
+  avatar: string;
+}
+
+const Home = (props: HomeProps) => {
+  return (
+    <ChallengesProvider
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.challengesCompleted}
+    >
+      <div className={styles.container}>
+        <Head>
+          <title>Início | move.it</title>
+        </Head>
+
+        <ExperienceBar />
+
+        <CountdownProvider>
+          <section>
+            <div>
+              <ProfileProvider username={props.username} avatar={props.avatar}>
+                <Profile />
+              </ProfileProvider>
+              <CompletedChallenges />
+              <Countdown />
+            </div>
+
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
+      </div>
+    </ChallengesProvider>
+  );
+};
+
+export default Home;
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const {
+    level,
+    currentExperience,
+    challengesCompleted,
+    username,
+    avatar,
+  } = ctx.req.cookies;
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted),
+      username: String(username),
+      avatar: String(avatar),
+    },
+  };
+};
