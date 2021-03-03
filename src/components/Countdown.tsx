6d778d68aef2a14ef5963d241d63f319ebdfc766
Thales Macena <thalesmacena@gmail.com>
2021-03-02 23:35:29 -0300

    Add countdown component

---
+++ src/components/Countdown.tsx
@@ -0,0 +1,66 @@
+import { useContext } from 'react';
+import { CountdownContext } from '../contexts/CountdownContext';
+import styles from '../styles/components/Countdown.module.css';
+
+export const Countdown = () => {
+  const {
+    hasFinished,
+    isActive,
+    resetCountdown,
+    startCountdown,
+    progress,
+    minutes,
+    seconds,
+  } = useContext(CountdownContext);
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+  const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+
+  return (
+    <>
+      <div className={styles.countdownContainer}>
+        <div>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
+        </div>
+
+        <span>:</span>
+
+        <div>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
+        </div>
+      </div>
+
+      {hasFinished ? (
+        <>
+          <button disabled type="button" className={styles.countdownButton}>
+            Ciclo Encerrado
+            <img src="icons/confirm.svg" alt="Level" />
+          </button>
+        </>
+      ) : (
+        <>
+          {isActive ? (
+            <button
+              type="button"
+              className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+              onClick={resetCountdown}
+            >
+              Abandonar ciclo
+            </button>
+          ) : (
+            <button
+              type="button"
+              className={styles.countdownButton}
+              onClick={startCountdown}
+            >
+              Iniciar um Ciclo
+            </button>
+          )}
+        </>
+      )}
+      <div className={styles.finishedBar} style={{ width: `${progress}%` }} />
+    </>
+  );
+};
---
+++ src/contexts/CountdownContext.tsx
@@ -0,0 +1,83 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable prettier/prettier */
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState
+} from 'react';
+import { ChallengesContext } from './ChallengesContext';
+
+interface CountdownContextData {
+  hasFinished: boolean;
+  isActive: boolean;
+  resetCountdown: () => void;
+  startCountdown: () => void;
+  progress: number;
+  minutes: number;
+  seconds: number;
+}
+
+export const CountdownContext = createContext({} as CountdownContextData);
+
+interface CountdownProviderProps {
+  children: ReactNode;
+}
+
+// eslint-disable-next-line no-undef
+let countdownTimeout: NodeJS.Timeout;
+
+export const CountdownProvider = ({ children }: CountdownProviderProps) => {
+  const { startNewChallenge } = useContext(ChallengesContext);
+
+  const [time, setTime] = useState(25 * 60);
+  const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
+  const [progress, setProgress] = useState(0);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  const startCountdown = () => {
+    setIsActive(true);
+  };
+
+  const resetCountdown = () => {
+    clearTimeout(countdownTimeout);
+    setIsActive(false);
+    setTime(25 * 60);
+    setHasFinished(false);
+    setProgress(0);
+  };
+
+  useEffect(() => {
+    if (isActive && time > 0) {
+      countdownTimeout = setTimeout(() => {
+        setTime(time - 1);
+        setProgress((1500 - time) / 15);
+      }, 1000);
+    } else if (isActive && time === 0) {
+      setHasFinished(true);
+      setIsActive(false);
+      setProgress(100);
+      startNewChallenge();
+    }
+  }, [isActive, time, startNewChallenge]);
+
+  return (
+    <CountdownContext.Provider
+      value={{
+        hasFinished,
+        isActive,
+        resetCountdown,
+        startCountdown,
+        progress,
+        minutes,
+        seconds,
+      }}
+    >
+      {children}
+    </CountdownContext.Provider>
+  );
+};
