f218af44908c9878509ff4ac0662c45748ff1ea9
Thales Macena <thalesmacena@gmail.com>
2021-03-02 23:33:47 -0300

    add Profile on Home Page

---
+++ src/components/Profile.tsx
@@ -0,0 +1,22 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import { ProfileContext } from '../contexts/ProfileContext';
+import styles from '../styles/components/Profile.module.css';
+
+export const Profile = () => {
+  const { level } = useContext(ChallengesContext);
+  const { username, avatar } = useContext(ProfileContext);
+
+  return (
+    <div className={styles.profileContainer}>
+      <img src={avatar} alt={username} />
+      <div>
+        <strong>{username}</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level {level}
+        </p>
+      </div>
+    </div>
+  );
+};
