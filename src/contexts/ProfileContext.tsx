b8b98121147d74cb0cc54650c596076280d6cd87
Thales Macena <thalesmacena@gmail.com>
2021-03-02 23:30:49 -0300

    Create Profile Context and index page

---
+++ src/contexts/ProfileContext.tsx
@@ -0,0 +1,84 @@
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import { useRouter } from 'next/router';
+import { createContext, ReactNode, useEffect, useState } from 'react';
+
+interface ProfileContextData {
+  login: (loginUserName: string) => void;
+  username: string;
+  avatar: string;
+  isFound: boolean;
+  loading: boolean;
+  cleanError: () => void;
+}
+
+export const ProfileContext = createContext({} as ProfileContextData);
+
+interface ProfileProviderProps {
+  children: ReactNode;
+  username: string;
+  avatar: string;
+}
+
+export const ProfileProvider = ({
+  children,
+  ...rest
+}: ProfileProviderProps) => {
+  const [username, setUsername] = useState(rest.username ?? 'teste');
+  const [avatar, setAvatar] = useState(rest.avatar ?? 'teste');
+  const [isFound, setIsFound] = useState(true);
+  const [loading, setLoading] = useState(false);
+
+  const Router = useRouter();
+
+  useEffect(() => {
+    Cookies.set('username', String(username));
+    Cookies.set('avatar', String(avatar));
+  }, [username, avatar]);
+
+  const cleanError = () => {
+    setIsFound(true);
+  };
+
+  const login = async (LoginUserName: string) => {
+    let response = null;
+    setLoading(true);
+
+    try {
+      response = await axios.get(
+        `https://api.github.com/users/${LoginUserName}`
+      );
+    } catch {
+      setIsFound(false);
+      setLoading(false);
+      return;
+    }
+
+    if (response) {
+      setIsFound(true);
+
+      const { data } = response;
+      setUsername(data.name);
+      setAvatar(data.avatar_url);
+
+      Router.push('moveit');
+
+      setLoading(false);
+    }
+  };
+
+  return (
+    <ProfileContext.Provider
+      value={{
+        login,
+        username,
+        avatar,
+        isFound,
+        loading,
+        cleanError,
+      }}
+    >
+      {children}
+    </ProfileContext.Provider>
+  );
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,53 @@
+import { GetServerSideProps } from 'next';
+import { InputLogin } from '../components/InputLogin';
+import { ProfileProvider } from '../contexts/ProfileContext';
+import styles from '../styles/pages/Login.module.css';
+
+interface LoginProps {
+  username: string;
+  avatar: string;
+}
+
+const Login = (props: LoginProps) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        <img src="/logo.svg" alt="Moove.it" />
+      </div>
+
+      <div className={styles.containerRight}>
+        <div className={styles.containerLogo}>
+          <img src="/logo-full-white.svg" alt="Moove.it" />
+        </div>
+
+        <div className={styles.containerLogin}>
+          <strong>Bem-Vindo</strong>
+
+          <div className={styles.containerGithub}>
+            <img src="/icons/github.svg" alt="Github" />
+            Faça Login com seu Github
+            <br />
+            para começar
+          </div>
+
+          <ProfileProvider username={props.username} avatar={props.avatar}>
+            <InputLogin />
+          </ProfileProvider>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { username, avatar } = ctx.req.cookies;
+
+  return {
+    props: {
+      username: String(username),
+      avatar: String(avatar),
+    },
+  };
+};
